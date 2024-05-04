66ef98ddbac6ca012164d3e536290b60e230c576
RoPeak <Ronanpeacock2004@gmail.com>
2024-05-04 18:28:47 +0100

    Abstraction into seperate functions

--- src/script.js
+++ src/script.js
@@ -1,27 +1,27 @@
-window.onload = createGrid;
-window.onresize = createGrid;
+// Main variables
+const squareSize = 30;
+const marginSize = 200;
+const grid = document.getElementById('grid');
+let gridArr;
 
-function createGrid() {
-    // Clear existing squares
-    const grid = document.getElementById('grid')
-    while (grid.firstChild) {
-        grid.firstChild.remove();
-    }
+window.onload = mainLogic;
+window.onresize = mainLogic;
 
-    // Setup grid
-    const squareSize = 30;
-    const marginSize = 200;
+function mainLogic() {
+    // Calculate how many squares are needed
     const numSquaresX = Math.ceil((window.innerWidth - marginSize) / squareSize);
-    const numSquaresY = Math.ceil((window.innerHeight -  marginSize) / squareSize);
-    const totalSquares = numSquaresX * numSquaresY;
+    const numSquaresY = Math.ceil((window.innerHeight -  marginSize) / squareSize);    
 
     // Adjust the grid to fit the exact number of squares
     grid.style.gridTemplateColumns = `repeat(${numSquaresX}, ${squareSize}px)`;
     grid.style.gridTemplateRows = `repeat(${numSquaresY}, ${squareSize}px)`;
+    
+    // Create and draw the grid
+    gridArr = createGrid(numSquaresX, numSquaresY);
+    drawGrid(gridArr);
+}
 
-    console.log(numSquaresX);
-    console.log(numSquaresY);
-
+function createGrid(numSquaresX, numSquaresY) {
     var gridArr = new Array(numSquaresX);
     for (let i = 0; i < gridArr.length; i++) {
         gridArr[i] = new Array(numSquaresY);
@@ -29,10 +29,24 @@ function createGrid() {
             gridArr[i][j] = 0;
         }
     }
+    return gridArr
+}
 
-    for (let i = 0; i < totalSquares; i++) {
-        const square = document.createElement('div');
-        square.className = 'square';
-        grid.appendChild(square);
+function drawGrid(gridArr) {
+    // Clear the grid
+    while (grid.firstChild) {
+        grid.firstChild.remove();
+    }
+
+    // Create the squares
+    for (let i = 0; i < gridArr.length; i++) {
+        for (let j = 0; j < gridArr[i].length; j++) {
+            const square = document.createElement('div');
+            square.className = 'square';
+            if (gridArr[i][j] === 1) {
+                square.classList.add('sand');
+            }
+            grid.appendChild(square);
+        }
     }
 }
