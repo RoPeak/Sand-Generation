61ded8b9cf1b3a505b741b969b2cefd83c51fa57
RoPeak <Ronanpeacock2004@gmail.com>
2024-05-04 17:42:42 +0100

    Dynamic grid generation

--- src/script.js
+++ src/script.js
@@ -1,10 +1,23 @@
-window.onload = function() {
-    // Setup grid
+window.onload = createGrid;
+window.onresize = createGrid;
+
+function createGrid() {
+    // Clear existing squares
     const grid = document.getElementById('grid')
+    while (grid.firstChild) {
+        grid.firstChild.remove();
+    }
+
+    // Setup grid
     const squareSize = 30;
-    const numSquaresX = Math.ceil(window.innerWidth / squareSize);
-    const numSquaresY = Math.ceil(window.innerHeight / squareSize);
-    const totalSquares = numSquaresX + numSquaresY;
+    const marginSize = 100;
+    const numSquaresX = Math.ceil((window.innerWidth - 2 * marginSize) / squareSize);
+    const numSquaresY = Math.ceil((window.innerHeight - 2 * marginSize) / squareSize);
+    const totalSquares = numSquaresX * numSquaresY;
+
+    // Adjust the grid to fit the exact number of squares
+    grid.style.gridTemplateColumns = `repeat(${numSquaresX}, ${squareSize}px)`;
+    grid.style.gridTemplateRows = `repeat(${numSquaresY}, ${squareSize}px)`;
 
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
