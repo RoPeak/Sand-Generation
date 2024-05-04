f959748137e604045678747a6a123302d754e4ba
RoPeak <Ronanpeacock2004@gmail.com>
2024-05-04 17:59:41 +0100

    Matrix setup

--- src/script.js
+++ src/script.js
@@ -10,14 +10,25 @@ function createGrid() {
 
     // Setup grid
     const squareSize = 30;
-    const marginSize = 100;
-    const numSquaresX = Math.ceil((window.innerWidth - 2 * marginSize) / squareSize);
-    const numSquaresY = Math.ceil((window.innerHeight - 2 * marginSize) / squareSize);
+    const marginSize = 200;
+    const numSquaresX = Math.ceil((window.innerWidth - marginSize) / squareSize);
+    const numSquaresY = Math.ceil((window.innerHeight -  marginSize) / squareSize);
     const totalSquares = numSquaresX * numSquaresY;
 
     // Adjust the grid to fit the exact number of squares
     grid.style.gridTemplateColumns = `repeat(${numSquaresX}, ${squareSize}px)`;
     grid.style.gridTemplateRows = `repeat(${numSquaresY}, ${squareSize}px)`;
+
+    console.log(numSquaresX);
+    console.log(numSquaresY);
+
+    var gridArr = new Array(numSquaresX);
+    for (let i = 0; i < gridArr.length; i++) {
+        gridArr[i] = new Array(numSquaresY);
+        for (let j = 0; j < gridArr[i].length; j++) {
+            gridArr[i][j] = 0;
+        }
+    }
 
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
